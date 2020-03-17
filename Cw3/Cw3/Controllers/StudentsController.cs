6ea47a48aa155b22ab805fe630857dd7cb7eb883
Mateusz Godlewski <s19282@pjwstk.edu.pl>
2020-03-17 10:03:00 +0100

    Zadanie 7

--- Cw3/Cw3/Controllers/StudentsController.cs
+++ Cw3/Cw3/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cw3.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -26,6 +27,33 @@ namespace Cw3.Controllers
             else if(id ==2)
             {
                 return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpPost]
+        public IActionResult createStudent(Student student)
+        {
+            student.indexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult putStudent(int id)
+        {
+            if(id==1)
+            {
+                return Ok("Aktualizacja dokończona");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult deleteStudent(int id)
+        {
+            if(id==2)
+            {
+                return Ok("Usuwanie ukończone");
             }
             return NotFound("Nie znaleziono studenta");
         }
