bb942dd8063f053e4e089e969a9a6e15c1310399
Mateusz Godlewski <s19282@pjwstk.edu.pl>
2020-03-20 10:37:48 +0100

    Zadanie 3

---
+++ Cw03/Cw03/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw03.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudent()
+        {
+            return "Kowalski, Malewski, Andrzejewski";
+        }
+    }
+}
