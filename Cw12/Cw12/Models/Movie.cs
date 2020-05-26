9650a608d54f8c130a59fadd46b6d201c77547a3
Mateusz Godlewski <s19282@pjwstk.edu.pl>
2020-05-26 18:07:04 +0200

    Dodawanie wyszukiwania

--- Cw12/Cw12/Models/Movie.cs
+++ Cw12/Cw12/Models/Movie.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Cw12.Models
 {
@@ -10,9 +11,11 @@ namespace Cw12.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
