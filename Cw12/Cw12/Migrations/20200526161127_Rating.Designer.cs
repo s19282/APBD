f03adec4376f4ebae0665f5ee8a83bc936461c94
Mateusz Godlewski <s19282@pjwstk.edu.pl>
2020-05-26 18:15:38 +0200

    Dodawanie nowego pola

--- Cw12/Cw12/Migrations/20200526161127_Rating.Designer.cs
+++ Cw12/Cw12/Migrations/20200526161127_Rating.Designer.cs
@@ -4,14 +4,16 @@ using Cw12.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Cw12.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    partial class MvcMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20200526161127_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
