e0aa12a70dd82f8ff67a81480711f22c8ee0f2af
Mateusz Godlewski <s19282@pjwstk.edu.pl>
2020-05-26 09:17:08 +0200

    Dodawanie kontrolera

---
+++ Cw12/Cw12/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Cw12.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        //
+        // GET: /HelloWorld/Welcome/
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+
+    }
+}
